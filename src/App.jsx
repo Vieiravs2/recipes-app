21bad97e5c84116f14fd6f2f266e0958240f325b
Michel Matos <michelsys@hotmail.com>
2023-05-05 17:54:58 -0400

    feat: requisito 06.
    Redireciona para tela principal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
+import Meals from './pages/Meals';
 
 function App() {
   return (
-    <Login />
+    <Switch>
+      <Route exact path="/" component={ Login } />
+      <Route path="/meals" component={ Meals } />
+    </Switch>
   );
 }
 
