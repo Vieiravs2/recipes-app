02dda7f5595fc2046ee5ce3095be0506bcc5e886
Michel Matos <michelsys@hotmail.com>
2023-05-05 18:27:58 -0400

    feat: requisito 06.
    Foram criados todas as rotas e componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import './App.css';
-import Meals from './pages/Meals';
+import Recipes from './pages/Recipes';
+import Profile from './pages/Profile';
+import DoneRecipes from './pages/DoneRecipes';
+import FavoriteRecipes from './pages/FavoriteRecipes';
+import RecipeDetails from './pages/RecipeDetails';
+import RecipeInProgress from './pages/RecipeInProgress';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/" component={ Login } />
-      <Route path="/meals" component={ Meals } />
+      <Route path="/meals" component={ Recipes } />
+      <Route path="/drinks" component={ Recipes } />
+      <Route path="/meals/:id-da-receita" component={ RecipeDetails } />
+      <Route path="/drinks/:id-da-receita" component={ RecipeDetails } />
+      <Route path="/meals/:id-da-receita/in-progress" component={ RecipeInProgress } />
+      <Route path="/drinks/:id-da-receita/in-progress" component={ RecipeInProgress } />
+      <Route path="/profile" component={ Profile } />
+      <Route path="/done-recipes" component={ DoneRecipes } />
+      <Route path="/favorite-recipes" component={ FavoriteRecipes } />
     </Switch>
   );
 }
