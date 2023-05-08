96d35a465370562d003d067d63b7eafcfafac403
rbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-08 17:23:36 -0300

    test: requisito 16 e 17
    verifica se os elementos estão no documento

---
+++ src/tests/Footer.test.jsx
@@ -0,0 +1,30 @@
+import { screen, render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
+import LoginProvider from '../providers/LoginProvider';
+import App from '../App';
+
+describe('', () => {
+  it('renderiza o footer corretamente', () => {
+    render(
+      <BrowserRouter>
+        <LoginProvider>
+          <App />
+        </LoginProvider>
+      </BrowserRouter>,
+    );
+
+    const emailInput = screen.getByRole('textbox');
+    const passwordInput = screen.getByPlaceholderText(/senha/i);
+    const btn = screen.getByRole('button');
+    userEvent.type(emailInput, 'email@test.com');
+    userEvent.type(passwordInput, '1234567');
+    userEvent.click(btn);
+
+    const mealBtn = screen.getByAltText('meal-icon');
+    expect(mealBtn).toBeInTheDocument();
+
+    const drinkBtn = screen.getByAltText('drink-icon');
+    expect(drinkBtn).toBeInTheDocument();
+  });
+});
