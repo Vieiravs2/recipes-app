1a263e3eb7fff3206d4967d7d6af4c71d3658131
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-05-08 15:42:48 -0300

    Feat: Requisito 11
    Adicionado uma busca corretamente conforme o radio selecionado

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,11 +1,50 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 export default function SearchBar() {
   const [searchValue, setSearchValue] = useState('');
-  const [selectedOption, setSelectedOption] = useState('Ingredient');
+  const [selectedOption, setSelectedOption] = useState('');
+  const [responseAPI, setResponseAPI] = useState('');
 
-  const handleOptionChange = (event) => {
-    setSelectedOption(event.target.value);
+  const searchIngredientAPI = async () => {
+    const getAPI = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchValue}`);
+    const response = await getAPI.json();
+    return response;
+  };
+
+  const searchNameAPI = async () => {
+    const getAPI = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchValue}`);
+    const response = await getAPI.json();
+    return response;
+  };
+
+  const searchFirstLetterAPI = async () => {
+    if (searchValue.length === 1) {
+      const getAPI = await fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${searchValue}`);
+      const response = await getAPI.json();
+      return response;
+    }
+    return global.alert('Your search must have only 1 (one) character');
+  };
+
+  useEffect(() => {
+    switch (selectedOption) {
+    case 'Ingredient':
+      setResponseAPI(searchIngredientAPI());
+      break;
+    case 'Name':
+      setResponseAPI(searchNameAPI());
+      break;
+    case 'First letter':
+      setResponseAPI(searchFirstLetterAPI());
+      break;
+    default:
+      break;
+    }
+  }, [responseAPI, searchValue, selectedOption]);
+
+  const handleOptionChange = ({ target }) => {
+    const { value } = target;
+    setSelectedOption(value);
   };
 
   const handleChange = ({ target: { value } }) => {
@@ -22,8 +61,9 @@ export default function SearchBar() {
       />
       <label>
         <input
-          type="text"
+          type="radio"
           data-testid="ingredient-search-radio"
+          name="option"
           value="Ingredient"
           checked={ selectedOption === 'Ingredient' }
           onChange={ handleOptionChange }
@@ -32,18 +72,9 @@ export default function SearchBar() {
       </label>
       <label>
         <input
-          type="text"
-          data-testid="ingredient-search-radio"
-          value="Ingredient"
-          checked={ selectedOption === 'Ingredient' }
-          onChange={ handleOptionChange }
-        />
-        Ingredient
-      </label>
-      <label>
-        <input
-          type="text"
+          type="radio"
           data-testid="name-search-radio"
+          name="option"
           value="Name"
           checked={ selectedOption === 'Name' }
           onChange={ handleOptionChange }
@@ -52,8 +83,9 @@ export default function SearchBar() {
       </label>
       <label>
         <input
-          type="text"
+          type="radio"
           data-testid="first-letter-search-radio"
+          name="option"
           value="First letter"
           checked={ selectedOption === 'First letter' }
           onChange={ handleOptionChange }
@@ -65,7 +97,6 @@ export default function SearchBar() {
         data-testid="exec-search-btn"
       >
         Buscar
-
       </button>
     </div>
   );
