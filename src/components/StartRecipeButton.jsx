20656fc4cdd9a9ecd052594409f4efeffd67d616
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-11 15:22:02 -0300

    feat: requisito 30.
    adicionando condicional de renderização do texto Start Recipe ou Continue Recipe.

--- src/components/StartRecipeButton.jsx
+++ src/components/StartRecipeButton.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import { useParams } from 'react-router-dom';
+import { useLocation, useParams } from 'react-router-dom';
 
 export default function StartRecipeButton() {
   const { id } = useParams();
+  const location = useLocation();
+  const { pathname } = location;
+  const mealOrDrinks = pathname.includes('/meals') ? 'meals' : 'drinks';
 
   let doneRecipesStoraged = JSON.parse(localStorage.getItem('doneRecipes'));
   if (doneRecipesStoraged === null) {
     doneRecipesStoraged = [];
   }
   const recipeIsDone = doneRecipesStoraged.some((doneRecipe) => doneRecipe.id === id);
+
+  let inProgressRecipesStoraged = JSON.parse(localStorage.getItem('inProgressRecipes'));
+  if (inProgressRecipesStoraged === null) {
+    inProgressRecipesStoraged = {
+      drinks: {},
+      meals: {},
+    };
+  }
+  if (inProgressRecipesStoraged.drinks === undefined) {
+    inProgressRecipesStoraged.drinks = {};
+  }
+  if (inProgressRecipesStoraged.meals === undefined) {
+    inProgressRecipesStoraged.meals = {};
+  }
+
+  const recipeIsInProgress = Object.keys(inProgressRecipesStoraged[mealOrDrinks])
+    .some((inProgressRecipeId) => inProgressRecipeId === id);
 
   return (
     !recipeIsDone && (
@@ -17,7 +37,7 @@ export default function StartRecipeButton() {
         data-testid="start-recipe-btn"
         style={ { position: 'fixed', bottom: 0, zIndex: 1 } }
       >
-        Start Recipe
+        {recipeIsInProgress ? 'Continue Recipe' : 'Start Recipe' }
       </button>
     )
   );
