a35b4bf072de9f0eaea9032f3bb10907bab442f5
Larissa <toledobruno610@gmail.com>
2023-05-11 14:39:49 -0300

    feat: resolve requisitos 57 a 62
    feito os requisitos 57 a 62, faltando somente os testes da página de profile

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,49 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 export default function Profile() {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  const handlerLogout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
   return (
     <>
       <Header title="Profile" profile search={ false } />
+      <section>
+        <h5 data-testid="profile-email">{user.email}</h5>
+      </section>
+      <section>
+        <Link to="/favorite-recipes">
+          <button
+            type="button"
+            data-testid="profile-favorite-btn"
+          >
+            Favorite Recipes
+          </button>
+        </Link>
+
+        <Link to="/done-recipes">
+          <button
+            type="button"
+            data-testid="profile-done-btn"
+          >
+            Done Recipes
+          </button>
+        </Link>
+        <Link to="/">
+          <button
+            type="button"
+            data-testid="profile-logout-btn"
+            onClick={ () => handlerLogout() }
+          >
+            Logout
+          </button>
+        </Link>
+      </section>
       <Footer />
     </>
   );
