998eb9440d654d46121b81949f2b5009fa6f3cbc
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-11 18:05:17 -0300

    feat: requisito 32.
    adicionando solução para compartilhar o link da receita e avisar qeu o link foi copiado para o clipboard.

--- src/components/ShareButton.jsx
+++ src/components/ShareButton.jsx
@@ -1,12 +1,32 @@
+import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import shareIcon from '../images/shareIcon.svg';
 
+const copy = require('clipboard-copy');
+
+const TWO_SECONDS = 2000;
+
 function ShareButton() {
+  const history = useHistory();
+  const { pathname } = history.location;
+  const [linkCopied, setLinkCopied] = useState(false);
+
+  const shareLink = () => {
+    const link = `http://localhost:3000${pathname}`;
+    copy(link);
+    setTimeout(() => setLinkCopied(false), TWO_SECONDS);
+    setLinkCopied(true);
+  };
   return (
-    <button
-      data-testid="share-btn"
-    >
-      <img src={ shareIcon } alt="share-icon" />
-    </button>
+    <>
+      <button
+        data-testid="share-btn"
+        onClick={ () => shareLink() }
+      >
+        <img src={ shareIcon } alt="share-icon" />
+      </button>
+      {linkCopied && <span>Link copied!</span>}
+    </>
   );
 }
 
