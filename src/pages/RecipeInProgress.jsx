adc5cb44e1d1a09040bf0e65b90294fa65fa3811
Thiago Banfi Credico <hbanfi3@hotmail.com>
2023-05-12 18:59:57 -0300

    feat: requisito 37.
    Desenvolvimento da tela de receita em progresso.

--- src/pages/RecipeInProgress.jsx
+++ src/pages/RecipeInProgress.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import RecipesDetails from './RecipeDetails';
+import FinishRecipeButton from '../components/FinishRecipeButton';
 
 export default function RecipeInProgress() {
   return (
-    <div>RecipeInProgress</div>
+    // <div>RecipeInProgress</div>
+    <>
+      <RecipesDetails />
+      <FinishRecipeButton />
+    </>
   );
 }
