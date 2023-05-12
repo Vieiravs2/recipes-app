3eb1fc9d8997ab5abb6c09243677d2b6c7a256bd
Vieiravs2 <matheusemidio.canal@gmail.com>
2023-05-12 18:56:34 -0300

    Feat: Requisito 52
    Implementado span que diz se a bebida é alcoolica ou não

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
+import blackHeartIcon from '../images/blackHeartIcon.svg';
 
 export default function FavoriteRecipes() {
   const [stateFavoriteRecipes, setStateFavoriteRecipes] = useState([]);
@@ -16,37 +18,58 @@ export default function FavoriteRecipes() {
         <button data-testid="filter-by-all-btn">All</button>
         <button data-testid="filter-by-meal-btn">Meals</button>
         <button data-testid="filter-by-drink-btn">Drinks</button>
-        { stateFavoriteRecipes.map((element, index) => (
+        {stateFavoriteRecipes.map((element, index) => (
           <div key={ element.id } className="cards_favorite_recipes">
-            <img
-              src={ element.image }
-              alt={ element.name }
-              data-testid={ `${index}-horizontal-image` }
-            />
-            <span data-testid={ `${index}-horizontal-top-text` }>
-              {`${element.nationality} - ${element.category}`}
-            </span>
-            <span data-testid={ `${index}-horizontal-name` }>
-              {element.name}
-            </span>
-            <button data-testid={ `${index}-horizontal-share-btn` }>
-              Compartilhar
-            </button>
-            <button data-testid={ `${index}-horizontal-favorite-btn` }>
-              Favoritar
-            </button>
+            {element.type === 'meal' ? (
+              <>
+                <img
+                  src={ element.image }
+                  alt={ element.name }
+                  data-testid={ `${index}-horizontal-image` }
+                />
+                <span data-testid={ `${index}-horizontal-name` }>
+                  {element.name}
+                </span>
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  {`${element.nationality} - ${element.category}`}
+                </span>
+                <button src={ shareIcon } data-testid={ `${index}-horizontal-share-btn` }>
+                  <img src={ shareIcon } alt="share-icon" />
+                </button>
+                <button
+                  src={ blackHeartIcon }
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                >
+                  <img src={ blackHeartIcon } alt="favorite-icon" />
+                </button>
+              </>
+            ) : (
+              <>
+                <img
+                  src={ element.image }
+                  alt={ element.name }
+                  data-testid={ `${index}-horizontal-image` }
+                />
+                <span data-testid={ `${index}-horizontal-top-text` }>
+                  {element.alcoholicOrNot ? element.alcoholicOrNot : 'No'}
+                </span>
+                <span data-testid={ `${index}-horizontal-name` }>
+                  {element.name}
+                </span>
+                <button src={ shareIcon } data-testid={ `${index}-horizontal-share-btn` }>
+                  <img src={ shareIcon } alt="share-icon" />
+                </button>
+                <button
+                  src={ blackHeartIcon }
+                  data-testid={ `${index}-horizontal-favorite-btn` }
+                >
+                  <img src={ blackHeartIcon } alt="favorite-icon" />
+                </button>
+              </>
+            )}
           </div>
-        )) }
+        ))}
       </div>
     </div>
   );
 }
-
-// O botão de filtro `All` deve ter o atributo `data-testid="filter-by-all-btn"`;
-// * O botão de filtro `Meals` deve ter o atributo `data-testid="filter-by-meal-btn"`;
-// * O botão de `Drinks` deve ter o atributo `data-testid="filter-by-drink-btn"`;
-// * A imagem do card de receita deve ter o atributo `data-testid="${index}-horizontal-image"`;
-// * O texto da categoria da receita deve ter o atributo `data-testid="${index}-horizontal-top-text"`;
-// * O texto do nome da receita deve ter o atributo `data-testid="${index}-horizontal-name"`;
-// * O elemento de compartilhar a receita deve ter o atributo `data-testid="${index}-horizontal-share-btn"`;
-// * O elemento de favoritar a receita deve ter o atributo `data-testid="${index}-horizontal-favorite-btn"`;
