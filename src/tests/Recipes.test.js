af26b35929ec8643b6b4c11f4aa000b0b261606b
Michel Matos <michelsys@hotmail.com>
2023-05-13 23:30:19 -0400

    Feat: Requisito 22 testes 100% .

--- src/tests/Recipes.test.js
+++ src/tests/Recipes.test.js
@@ -1,60 +1,65 @@
-import { screen, waitFor } from '@testing-library/react';
+import { screen } from '@testing-library/react';
+import { act } from 'react-dom/test-utils';
+import userEvent from '@testing-library/user-event';
 import LoginProvider from '../providers/LoginProvider';
 import Recipes from '../pages/Recipes';
 import renderWithRouter from '../helpers/renderWithRouter';
 import FetchProvider from '../providers/FetchProvider';
-import { mockMaels } from './mocks/mockMeals';
-import { mockMealCategories } from './mocks/mockMealCategory';
-// import { mockCategoryBeef } from './mocks/mockCategoryBeef';
-// import { mockDrinks } from './mocks/mockDrinks';
-// import { mockDrinkCategories } from './mocks/mockDrinkCategoty';
+import fetch from '../../cypress/mocks/fetch';
 
 describe('Testa a página de receitas', () => {
-  beforeEach(() => {
-    global.fetch = jest.fn()
-      .mockResolvedValueOnce({
-        json: jest.fn().mockResolvedValue(mockMaels),
-      })
-      .mockResolvedValueOnce({
-        json: jest.fn().mockResolvedValue(mockMealCategories),
-      });
-  });
+  beforeEach(async () => {
+    global.fetch = jest.fn().mockImplementation(fetch);
 
-  it('Testa se a página de receitas é renderizada', async () => {
-    renderWithRouter(
-      <FetchProvider>
-        <LoginProvider>
-          <Recipes />
-        </LoginProvider>
-      </FetchProvider>,
-      '/meals',
-    );
-
-    await waitFor(() => {
-      for (let index = 0; index < 12; index += 1) {
-        expect(screen.getByTestId(`${index}-recipe-card`)).toBeInTheDocument();
-      }
-      expect(screen.getByRole('button', { name: /beef/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /breakfast/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /chicken/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /dessert/i })).toBeInTheDocument();
-      expect(screen.getByRole('button', { name: /goat/i })).toBeInTheDocument();
+    await act(async () => {
+      renderWithRouter(
+        <FetchProvider>
+          <LoginProvider>
+            <Recipes />
+          </LoginProvider>
+        </FetchProvider>,
+        '/meals',
+      );
     });
   });
 
-  it('testa o filtro de categorias', async () => {
-    renderWithRouter(
-      <FetchProvider>
-        <LoginProvider>
-          <Recipes />
-        </LoginProvider>
-      </FetchProvider>,
-      '/meals',
-    );
-    await waitFor(() => {
-      expect(screen.getByTestId('10-card-name')).toBeInTheDocument();
-      // userEvent.click(beefButton);
-      screen.debug();
+  it('testa o filtro da categoria Beef', async () => {
+    const ENDPOINT_TO_REQUEST = 'https://www.themealdb.com/api/json/v1/1/filter.php?c=Beef';
+
+    await act(async () => {
+      const categoryBeef = screen.getByTestId('Beef-category-filter');
+      expect(categoryBeef).toBeInTheDocument();
+      userEvent.click(categoryBeef);
     });
+
+    expect(global.fetch).toBeCalledWith(ENDPOINT_TO_REQUEST);
+  });
+
+  it('testa o se o botão redireciona para pagina Drinks', async () => {
+    const ENDPOINT_TO_REQUEST = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?c=list';
+
+    await act(async () => {
+      const buttonDrink = screen.getByTestId('drinks-bottom-btn');
+      userEvent.click(buttonDrink);
+    });
+
+    expect(global.fetch).toBeCalledWith(ENDPOINT_TO_REQUEST);
+  });
+
+  it('testa o filtro da categoria Ordinary Drink', async () => {
+    const ENDPOINT_TO_REQUEST = 'https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary Drink';
+
+    await act(async () => {
+      const buttonDrink = screen.getByTestId('drinks-bottom-btn');
+      userEvent.click(buttonDrink);
+    });
+
+    await act(async () => {
+      const categoryOrdinaryDrink = screen.getByTestId('Ordinary Drink-category-filter');
+      expect(categoryOrdinaryDrink).toBeInTheDocument();
+      userEvent.click(categoryOrdinaryDrink);
+    });
+
+    expect(global.fetch).toBeCalledWith(ENDPOINT_TO_REQUEST);
   });
 });
