9e2b83602be0005b02bd0341f6e3f949bf2d6ab0
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-15 06:14:31 -0300

    feat: requisito 44
    Renderiza os cards das receitas feitas do local storage.

--- src/pages/DoneRecipes.jsx
+++ src/pages/DoneRecipes.jsx
@@ -1,8 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/Header';
+import shareIcon from '../images/shareIcon.svg';
 
 export default function DoneRecipes() {
+  const [doneRecipes, setDoneRecipes] = useState([]);
+
+  useEffect(() => {
+    const getRecipesFromLocalStorage = JSON
+      .parse(localStorage.getItem('doneRecipes')) || [];
+    setDoneRecipes(getRecipesFromLocalStorage);
+  }, []);
+
   return (
-    <Header title="Done Recipes" profile search={ false } />
+    <main>
+      <Header title="Done Recipes" profile search={ false } />
+      <section className="done__btn">
+        <button data-testid="filter-by-all-btn">All</button>
+        <button data-testid="filter-by-meal-btn">Meals</button>
+        <button data-testid="filter-by-drink-btn">Drinks</button>
+      </section>
+      {doneRecipes.map((recipe, index) => (
+        <article className="done__card" key={ recipe.id }>
+          <img
+            src={ recipe.image }
+            alt={ recipe.name }
+            data-testid={ `${index}-horizontal-image` }
+          />
+          <h4 data-testid={ `${index}-horizontal-name` }>{recipe.name}</h4>
+          <span data-testid={ `${index}-horizontal-top-text` }>{recipe.category}</span>
+          <p data-testid={ `${index}-horizontal-done-date` }>{recipe.doneDate}</p>
+          <div>
+            {recipe.tags.map((tag) => (
+              <span
+                data-testid={ `${index}-${tag}-horizontal-tag` }
+                key={ tag }
+              >
+                {tag}
+              </span>
+            ))}
+          </div>
+          <button data-testid={ `${index}-horizontal-share-btn` }>
+            <img src={ shareIcon } alt="share-icon" />
+          </button>
+        </article>
+      ))}
+    </main>
   );
 }
