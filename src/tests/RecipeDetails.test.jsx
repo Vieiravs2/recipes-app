4597365937b21e0c721b03aabc8cb7243ae45536
rnbarcellos <rodrigo.barcellos@outlook.com.br>
2023-05-15 16:12:26 -0300

    test: requisito 36
    Realiza testes com coverage de 90%

--- src/tests/RecipeDetails.test.jsx
+++ src/tests/RecipeDetails.test.jsx
@@ -2,42 +2,83 @@ import { act } from 'react-dom/test-utils';
 import { screen } from '@testing-library/react';
 
 import renderWithRouter from '../helpers/renderWithRouter';
-import oneMeal from '../../cypress/mocks/oneMeal';
-import drinks from '../../cypress/mocks/drinks';
+import fetch from '../../cypress/mocks/fetch';
 
 import LoginProvider from '../providers/LoginProvider';
 import FetchProvider from '../providers/FetchProvider';
-import RecipesDetails from '../pages/RecipeDetails';
-
-const mockFetch = jest.spyOn(global, 'fetch');
-mockFetch.mockResolvedValueOnce({
-  json: () => Promise.resolve(oneMeal),
-});
-mockFetch.mockResolvedValueOnce({
-  json: () => Promise.resolve(drinks),
-});
+import App from '../App';
 
 describe('Casos de teste da página _RecipeDetails_', () => {
+  beforeEach(async () => {
+    global.fetch = jest.fn().mockImplementation(fetch);
+  });
+
   it('Renderiza as refeições corretamente', async () => {
-    const ENDPOINT = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=52771';
-    renderWithRouter(
-      <FetchProvider>
-        <LoginProvider>
-          <RecipesDetails />
-        </LoginProvider>
-      </FetchProvider>,
-      '/meals/52771',
-    );
-
-    screen.debug();
-
     await act(async () => {
-      const title = screen.getByRole('heading', {
-        name: /spicy arrabiata penne/i,
-      });
-      expect(title).toBeInTheDocument();
+      renderWithRouter(
+        <FetchProvider>
+          <LoginProvider>
+            <App />
+          </LoginProvider>
+        </FetchProvider>,
+        '/meals/52771',
+      );
     });
 
-    expect(global.fetch).toBeCalledWith(ENDPOINT);
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+
+    const mealImg = screen.getByRole('img', { name: /meal/i });
+    expect(mealImg).toBeInTheDocument();
+
+    const mealTitle = screen.getByRole('heading', { name: /spicy arrabiata penne/i });
+    expect(mealTitle).toBeInTheDocument();
+
+    const mealCategory = screen.getByText(/vegetarian/i);
+    expect(mealCategory).toBeInTheDocument();
+
+    const mealIngredient1 = screen.getByText(/penne rigate - 1 pound/i);
+    expect(mealIngredient1).toBeInTheDocument();
+
+    const mealIngredient8 = screen.getByText(/parmigiano-reggiano - spinkling/i);
+    expect(mealIngredient8).toBeInTheDocument();
+
+    const mealVideo = screen.getByTitle(/recipe-video/i);
+    expect(mealVideo).toBeInTheDocument();
+
+    const swiperCard1 = screen.getByTestId('0-recommendation-card');
+    expect(swiperCard1).toBeVisible();
+
+    const swiperCard2 = screen.getByTestId('1-recommendation-card');
+    expect(swiperCard2).toBeVisible();
+  });
+
+  it('Renderiza os drinks corretamente', async () => {
+    await act(async () => {
+      renderWithRouter(
+        <FetchProvider>
+          <LoginProvider>
+            <App />
+          </LoginProvider>
+        </FetchProvider>,
+        '/drinks/178319',
+      );
+    });
+
+    expect(global.fetch).toHaveBeenCalledTimes(2);
+
+    const drinkTitle = screen.getByRole('heading', { name: /aquamarine/i });
+    expect(drinkTitle).toBeInTheDocument();
+
+    const alcoholic = screen.getByText(/alcoholic/i);
+    expect(alcoholic).toBeInTheDocument();
+
+    const drinkIngredient1 = screen.getByText(/hpnotiq - 2 oz/i);
+    expect(drinkIngredient1).toBeInTheDocument();
+
+    const drinkIngredient3 = screen.getByText(/banana liqueur - 1 oz/i);
+    expect(drinkIngredient3).toBeInTheDocument();
+
+    const drinkDescription = screen.getByText(/shake well in a shaker with ice\. strain in a martini glass\./i);
+    expect(drinkDescription).toBeInTheDocument();
   });
 });
